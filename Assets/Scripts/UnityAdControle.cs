94a88faf3a626c984663afc7da67074a5d0b5ecd
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-06-14 10:54:54 -0300

    Criando o Script UnityAdControle

---
+++ Assets/Scripts/UnityAdControle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+/// <summary>
+/// Classe responsavel por gerenciar todos os anuncios (Ads) do jogo.
+/// </summary>
+public class UnityAdControle : MonoBehaviour
+{
+
+    public static bool showAds = true;
+
+    /// <summary>
+    /// Metodo responsavel por mostrar o anuncio (Ads). Esse metodo deve ser chamado no local que deseja ter anuncio.
+    /// </summary>
+    public static void ShowAd()
+    {
+    #if UNITY_ADS
+	    if(Advertisement.IsReady()){
+        //Mostra o anuncio - Ads
+	    Advertisement.Show();
+	}
+    #endif
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
