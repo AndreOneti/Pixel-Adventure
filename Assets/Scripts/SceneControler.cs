2a27ec3df3e8e085ed765bd5f61fed36df78d33d
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-06-14 10:31:53 -0300

    Script SceneControler refeito

--- Assets/Scripts/SceneControler.cs
+++ Assets/Scripts/SceneControler.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetString("currentScene", SceneManager.GetActiveScene().name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
